f93cce11d43aee973be3de1ad08184be71404cdf
Alex <velengura.alex>
2023-05-24 16:54:45 +0300

    created authReducer, registered it in module added interfaces

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,11 @@
+import { StoreModule } from '@ngrx/store';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { RegisterComponent } from './components/register/register.component';
+import { reducer } from './store/reducers';
 
 const routes: Routes = [
   {
@@ -12,7 +14,12 @@ const routes: Routes = [
   },
 ];
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(routes),
+    StoreModule.forFeature('auth', reducer),
+  ],
   declarations: [RegisterComponent],
 })
 export class AuthModule {}
