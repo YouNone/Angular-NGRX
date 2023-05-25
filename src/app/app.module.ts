61b44ff7ddc17d6ad9f3c9ec75a39009c090d121
Alex <velengura.alex>
2023-05-25 13:13:11 +0300

    added auth selector, fixed StoreDevtoolsModule error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from 'src/environments/environment';
+// import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -20,7 +20,7 @@ import { environment } from 'src/environments/environment';
     StoreModule.forRoot({}),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
-      logOnly: environment.production, // Restrict extension to log-only mode
+      logOnly: !isDevMode(),
       autoPause: true, // Pauses recording actions and state changes when the extension window is not open
       // trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
       // traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
