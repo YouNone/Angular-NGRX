cef3ea3d66f3c0c37321c798412ae0f018e35d50
Alex <velengura.alex>
2023-06-06 18:43:26 +0300

    mc-pagination in progress

--- src/app/shared/modules/feed/components/feed/feed.component.ts
+++ src/app/shared/modules/feed/components/feed/feed.component.ts
@@ -1,40 +1,65 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Store, select } from '@ngrx/store';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+
 import { getFeedAction } from '../../store/actions/getFeed.action';
-import { Observable } from 'rxjs';
 import { GetFeedResponseInterface } from '../../types/getFeedResponse.interface';
 import {
   errorSelector,
   feedSelector,
   isLoadingSelector,
 } from '../../store/selectors';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'mc-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.scss'],
 })
-export class FeedComponent implements OnInit {
+export class FeedComponent implements OnInit, OnDestroy {
   @Input('apiUrl') apiUrlProps: string;
 
   isLoading$: Observable<boolean>;
   error$: Observable<string | null>;
   feed$: Observable<GetFeedResponseInterface | null>;
+  limit = environment.limit;
+  baseUrl: string;
+  queryParamsSubsccribtion$: Subscription;
+  currentPage: number;
 
-  constructor(private store: Store) {}
+  constructor(
+    private store: Store,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.initializeValues();
     this.fetchData();
+    this.intitializeListeners;
   }
 
   initializeValues() {
     this.isLoading$ = this.store.pipe(select(isLoadingSelector));
     this.error$ = this.store.pipe(select(errorSelector));
     this.feed$ = this.store.pipe(select(feedSelector));
+    this.baseUrl = this.router.url.split('?')[0];
+  }
+
+  intitializeListeners() {
+    this.queryParamsSubsccribtion$ = this.route.queryParams.subscribe(
+      (params: Params) => {
+        this.currentPage = Number(params['page'] || '1');
+      }
+    );
   }
 
   fetchData() {
     this.store.dispatch(getFeedAction({ url: this.apiUrlProps }));
+  }
+
+  ngOnDestroy(): void {
+    this.queryParamsSubsccribtion$.unsubscribe();
   }
 }
